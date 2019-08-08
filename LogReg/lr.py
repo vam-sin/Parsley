3b70aa82516fd3788c7596d962f913712cf648f2
vamsi <vamsinallapareddy@gmail.com>
2019-08-08 10:13:08 +0530

    Log Reg model done+overfit

--- LogReg/lr.py
+++ LogReg/lr.py
@@ -2,12 +2,23 @@
 import pandas as pd
 from pandas import DataFrame
 import numpy as np
-import scipy as sp
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+stop_words = stopwords.words('english')
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
-from gensim.models import word2vec
-
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+from keras.models import Sequential
+from keras.layers import Embedding, Dense, LSTM, Flatten
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from nltk.stem import PorterStemmer
+stemmer= PorterStemmer()
+vectorizer = TfidfVectorizer()
+import nltk
 #Functions
 # Convert to binary
 def convert_to_bin(values):
@@ -20,6 +31,7 @@ def convert_to_bin(values):
 
     return bin
 
+
 # Import Train dataset
 train = pd.read_csv('../dataset/train2.tsv', delimiter='\t', encoding='utf-8')
 # print(train.shape) # 10239 statements with 16 columns
@@ -29,12 +41,29 @@ train = pd.read_csv('../dataset/train2.tsv', delimiter='\t', encoding='utf-8')
 
 # Test
 test = pd.read_csv('../dataset/test2.tsv', delimiter='\t', encoding='utf-8')
-x_test = test.iloc[:,[3,15]]
+x_test = test.iloc[:,3:4]
+x_test = np.asarray(x_test)
+x_test = x_test.tolist()
+X_test = []
+for d in x_test:
+    d = str(d[0])
+    X_test.append(d)
+
 y_test = test.iloc[:,2:3]
 y_test = np.asarray(y_test)
 
 # X and Y (Train)
-x_train = train.iloc[:,[3,15]]
+# x_train = train.iloc[:,[3,15]] (statement & justification)
+x_train = train.iloc[:, 3:4]
+x_train = np.asarray(x_train)
+x_train = x_train.tolist()
+X = []
+for d in x_train:
+    d = str(d[0])
+    X.append(d)
+# print(X)
+# x_train = np.asarray(x_train)
+# print(x_train)
 y_train = train.iloc[:,2:3]
 y_train = np.asarray(y_train)
 
@@ -51,13 +80,36 @@ y_test_binary = np.asarray(y_test_binary)
 le = preprocessing.LabelEncoder()
 y_train_binary = le.fit_transform(y_train_binary)
 y_test_binary = le.fit_transform(y_test_binary)
-# Input (Word Embeddings)
-vec_mod = gensim.models.KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin.gz", binary=True)
-vec_mod.init_sims(replace = True)
+# Input Word Embeddings
+vocab_size = 500
+encoded_docs = [one_hot(d, vocab_size) for d in X]
+max_length = 500
+padded_docs = pad_sequences(encoded_docs, maxlen = max_length, padding = 'post')
+print(padded_docs.shape)
+# Test
+vocab_size = 500
+encoded_docs_test = [one_hot(d, vocab_size) for d in X_test]
+max_length = 500
+padded_docs_test = pad_sequences(encoded_docs_test, maxlen = max_length, padding = 'post')
+
+# print(x_train[0][0])
+# print(text_process(x_train[0][0]))
+# for i in range(len(x_train)):
+#     for j in range(len(x_train[i])):
+#         x_train[i][j] = text_process(x_train[i][j])
+# TfIDf
+# Word2Vec
+# GloVe
+
 
 # Logistic Regression Model
-# model = LogisticRegression()
-# model.fit(x_train, y_train_binary)
-# print("Model Fitting Done")
-# predictions = model.predict(x_test)
-# print(confusion_matrix(y_test_binary, predictions))
+model = Sequential()
+model.add(Embedding(500, 32, input_length=500))
+model.add(Flatten())
+model.add(Dense(1, activation = 'sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+print(model.summary())
+
+model.fit(padded_docs, y_train_binary,validation_data=(padded_docs_test,y_test_binary), epochs=50, verbose=1)
+
+# Test data details
